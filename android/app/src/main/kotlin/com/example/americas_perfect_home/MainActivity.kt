a426074ae7c0c9075d586209062cdf1c95889b21
Pistote-Studio <peterjubitz@pistotestudio.com>
2025-11-10 15:13:17 -0800

    Nuke it

--- android/app/src/main/kotlin/com/example/americas_perfect_home/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.americas_perfect_home
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/com/psstudio/americas_perfect_home/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.psstudio.americas_perfect_home
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
