df915d2c08f61f999ffa749b6e6bb77e3b6f602c
Pistote-Studio <peterjubitz@pistotestudio.com>
2025-11-14 14:28:23 -0800

    fix const error

--- lib/screens/admin/create_job_page.dart
+++ lib/screens/admin/create_job_page.dart
@@ -5,7 +5,7 @@ class CreateJobPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text('Create Job'),
       ),
